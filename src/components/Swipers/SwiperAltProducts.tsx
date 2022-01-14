b10aa160590a4cee2e4e1dcb5eb0d13958b5b35e
Luboš Svoboda <lubos.svoboda@gmail.com>
2022-01-15 00:40:05 +0100

    SwiperAlt Jest fix

--- src/components/Swipers/SwiperAltProducts.tsx
+++ src/components/Swipers/SwiperAltProducts.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import CardProductAlt from '../Product/CardProductAlt'
-import { Swiper, SwiperSlide } from 'swiper/react'
+// import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Navigation } from 'swiper';
 import "swiper/css"
 import "swiper/css/navigation"
+import { Col, Row } from '../../layout/Grid';
 
 SwiperCore.use([Navigation])
 
 function SwiperAltProducts() {
     return (
-        <Swiper navigation={true} spaceBetween={10} breakpoints={{ "640": { "slidesPerView": 2, "spaceBetween": 20 }, "768": { "slidesPerView": 3 }, "992": { "slidesPerView": 4 } }}>
-            <SwiperSlide><CardProductAlt /></SwiperSlide>
-            <SwiperSlide><CardProductAlt /></SwiperSlide>
-            <SwiperSlide><CardProductAlt /></SwiperSlide>
-            <SwiperSlide><CardProductAlt /></SwiperSlide>
-            <SwiperSlide><CardProductAlt /></SwiperSlide>
-        </Swiper>
+        // <Swiper navigation={true} spaceBetween={10} breakpoints={{ "640": { "slidesPerView": 2, "spaceBetween": 20 }, "768": { "slidesPerView": 3 }, "992": { "slidesPerView": 4 } }}>
+        //     <SwiperSlide><CardProductAlt /></SwiperSlide>
+        //     <SwiperSlide><CardProductAlt /></SwiperSlide>
+        //     <SwiperSlide><CardProductAlt /></SwiperSlide>
+        //     <SwiperSlide><CardProductAlt /></SwiperSlide>
+        //     <SwiperSlide><CardProductAlt /></SwiperSlide>
+        // </Swiper>
+        <Row>
+            <Col size="col-md-4 mb-4">
+                <CardProductAlt />
+            </Col>
+            <Col size="col-md-4 mb-4">
+                <CardProductAlt />
+            </Col>
+        </Row>
     )
 }
 
